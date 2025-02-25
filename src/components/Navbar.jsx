9b05ffa6983a2552b55a3d070228bd17d35e2648
DMStha <109300299+DMStha@users.noreply.github.com>
2025-02-25 21:23:27 +0545

    minor changes in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,13 +10,16 @@ const MyNavbar = () => {
           <a href="">
             <img src={logo1} className="logo" alt="" />
           </a>
-          <Navbar.Brand href="#">Upex Move</Navbar.Brand>
+          <Navbar.Brand href="#">
+            <span className="px-1">Upex Move</span>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               <Nav.Link href="#">Home</Nav.Link>
               <Nav.Link href="#">About</Nav.Link>
               <Nav.Link href="#">Contact</Nav.Link>
+              <button className="btn-dark btn"> Signup</button>
             </Nav>
           </Navbar.Collapse>
         </div>
