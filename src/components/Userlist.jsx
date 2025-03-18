8c0590f456dfc13823536a1a6974919b897c3b77
DMStha <109300299+DMStha@users.noreply.github.com>
2025-03-19 00:16:36 +0545

    added users

--- src/components/Userlist.jsx
+++ src/components/Userlist.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 
 const Userlist = () => {
+  const users = [
+    { _id: 1, name: "Diom", address: "Kathmandu" },
+    { _id: 2, name: "Rahul", address: "Pokhara" },
+    { _id: 3, name: "Raju", address: "Pokhara" },
+    { _id: 4, name: "Raja", address: "Pokhara" },
+  ];
   return (
-    <div>
-      <h1>Users List</h1>
+    <div className=" container mt-4">
+      <h1>User List</h1>
+      <ul>
+        <li>
+          {users.map((users) => {
+            return <li key={users._id}>{users.name}</li>;
+          })}
+        </li>
+      </ul>
     </div>
   );
 };
